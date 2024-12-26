740598fba539e51b40b4ddf1fa427559ac77b4e2
Julien <julienmarg@gmail.com>
2024-12-26 14:30:31 +0100

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,45 @@
+/*Balayage multicolors*/
+
 #include "Adafruit_NeoPixel.h"
 #define PIN  13
-#define NUMPIXELS 15
+#define NUMPIXELS 144+15
 Adafruit_NeoPixel strip (NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int power=80;
 
 void setup() {
    strip.begin();
-   strip.setBrightness(255);  // luminosité de la LED (maximum 255)
+   strip.setBrightness(power);  // luminosité de la LED (maximum 255)
    Serial.begin(9600);
 }
 
 void set_light(int led){
     
-    int rouge = random(15, 256);
-    int vert = random(15, 256);
-    int bleu = random(15, 256);
+    int rouge = random(15, power);
+    int vert = random(15, power);
+    int bleu = random(15, power);
 
-    rouge= (rouge * rouge) / 255; // Augmente les valeurs
-    vert = (vert * vert) / 255;
-    bleu = (bleu * bleu) / 255;
+    rouge= (rouge * rouge) / power; // Augmente les valeurs
+    vert = (vert * vert) / power;
+    bleu = (bleu * bleu) / power;
 
     strip.setPixelColor(led, strip.Color(rouge, vert, bleu));
     strip.show();
-    delay(100);
+    delay(50);
 }
 
 void loop() {
 
-  for (int i =0;i < 15;i++){
+  for (int i =0;i < NUMPIXELS;i++){
     set_light(i);
   }
-    
-  for (int i =14;i > 0;i--){
+
+  strip.fill(0,0,0);
+
+  for (int i =NUMPIXELS-1;i > 0;i--){
     set_light(i);
   }
+
+  strip.fill(0,0,0);
 
 }
---
+++ test/main_balayage_multicolors.cpp
@@ -0,0 +1,39 @@
+/*Balayage multicolors*/
+
+#include "Adafruit_NeoPixel.h"
+#define PIN  13
+#define NUMPIXELS 15
+Adafruit_NeoPixel strip (NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+   strip.begin();
+   strip.setBrightness(255);  // luminosité de la LED (maximum 255)
+   Serial.begin(9600);
+}
+
+void set_light(int led){
+    
+    int rouge = random(15, 256);
+    int vert = random(15, 256);
+    int bleu = random(15, 256);
+
+    rouge= (rouge * rouge) / 255; // Augmente les valeurs
+    vert = (vert * vert) / 255;
+    bleu = (bleu * bleu) / 255;
+
+    strip.setPixelColor(led, strip.Color(rouge, vert, bleu));
+    strip.show();
+    delay(100);
+}
+
+void loop() {
+
+  for (int i =0;i < 15;i++){
+    set_light(i);
+  }
+    
+  for (int i =14;i > 0;i--){
+    set_light(i);
+  }
+
+}
