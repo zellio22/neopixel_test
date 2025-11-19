52a21f1403e0497987599fe2b386684f532186fc
zellio22 <julienmarg@gmail.com>
2025-11-19 16:01:46 +0100

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #define D0 12
 #define micro A0
 #define NUMPIXELS 15
+unsigned long time=0;
+String last_couleur;
 Adafruit_NeoPixel strip (NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 int val_mic =0;
 int power=255;
@@ -15,28 +17,56 @@ void setup() {
    Serial.begin(9600);
 }
 
-void set_colors(int red,int green, int bleu){
-    
-  for (int i =0;i < NUMPIXELS;i++){
-    strip.setPixelColor(i, strip.Color(red, green, bleu));
-    
-  }
-strip.show();
+void set_colors(int red,int green, int bleu,String couleur){
+    if (millis() >= time+1000 && couleur != last_couleur){
+        time=millis();
+        last_couleur=couleur;
+        Serial.print(val_mic);
+        Serial.println(" "+couleur);
+        for (int i =0;i < NUMPIXELS;i++){
+            strip.setPixelColor(i, strip.Color(red, green, bleu));
+        }
+    strip.show();
+    }
+
+
     
 }
 
 void loop() {
 
- 
- set_colors(0,255,0);
+
  delay(50);
 
 val_mic= analogRead(micro);
-Serial.println(val_mic);
-if (val_mic >= 525){
-    Serial.println(val_mic);
-    set_colors(255,0,0);
-    delay(1000);
+//Serial.println(val_mic);
+
+
+
+
+
+if (val_mic >= 540){
+
+    set_colors(255,0,0,"red");//rouge
+
+}
+else if (val_mic >= 530){
+
+    set_colors(255,165,0,"orange");//orange
+
+}
+else if (val_mic >= 520){
+
+    set_colors(255,255,0,"yel");//jaune
+
+}
+else if (val_mic >= 510){
+
+    set_colors(0,255,0,"green");//green
+}
+if (val_mic <= 500){
+
+    set_colors(0,128,0,"lite green");//petit green
 }
 
 }
